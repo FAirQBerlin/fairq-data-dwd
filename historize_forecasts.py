de4034e6403a439414794cb9a47a783652b7fd21
Jenkins Daemon <brother-mfc@inwt-statistics.de>
2025-07-03 16:03:56 +0200

    25.7.0

--- historize_forecasts.py
+++ historize_forecasts.py
@@ -6,7 +6,7 @@ because we won't know the actual weather values when making our forecasts.
 This script is used in a kubernetes job.
 """
 
-from datetime import datetime
+from datetime import datetime, timezone
 
 from fairq_data_dwd.date_time_utils import get_forecast_end_date, get_forecast_start_date
 from fairq_data_dwd.db_connect import send_data_clickhouse
@@ -17,7 +17,7 @@ end_date = get_forecast_end_date()
 start_date = get_forecast_start_date()
 
 df_for_db = retrieve_data_from_api(coordinates_list, start_date, end_date, "forecast")
-date_time_forecast = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
+date_time_forecast = datetime.now(tz=timezone.utc).strftime("%Y-%m-%d %H:00:00")
 df_for_db["date_time_forecast"] = date_time_forecast
 
 print(f"Sending data for period: {start_date} until {end_date} to clickhouse.")
